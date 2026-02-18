24fe44f330ab3c05f21bf4b3c9abb9fbab0a5a1a
Nagesh H <nagesh.h@oracle.com>
2026-02-18 20:41:34 +0530

    init

---
+++ src/test/java/in/n4smh/microservices/person/parent_webflux/PersonParentWebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package in.n4smh.microservices.person.parent_webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonParentWebFluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
